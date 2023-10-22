993ee51ed06ac5b88e3857922e5de0771e60694e
Mathi-MS <sammathi17@gmail.com>
2023-10-22 20:56:29 +0530

    day 1 added items

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//nav two-----------------------
+// Selecting icon
+var navtwoicon = document.getElementById("navtwoicon")
+var navcloseicon = document.getElementById("navcloseicon")
+var sidenav = document.getElementById("sidenav")
+
+navtwoicon.addEventListener("click",function(){
+    sidenav.style.visibility="visible "
+  })
+  navcloseicon.addEventListener("click",function(){
+    sidenav.style.visibility="hidden"
+  })
