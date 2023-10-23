9dac7ab3e7e37762aa1b28d0bf68bf00e3e2223d
Mathi-MS <sammathi17@gmail.com>
2023-10-23 19:21:46 +0530

    day2 updated sc

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,60 @@ navtwoicon.addEventListener("click",function(){
   })
 
 
+// works
+// Selecting Works id
+var workone = document.getElementById("workone")
+var worktwo = document.getElementById("worktwo")
+var workthree = document.getElementById("workthree")
+var workfour = document.getElementById("workfour")
+var workfive = document.getElementById("workfive")
+var worksix = document.getElementById("worksix")
+
+
+function alll() {
+  workone.setAttribute("src","assets/Images/work1.jpg")
+  worktwo.setAttribute("src","assets/Images/work2.jpg")
+  workthree.setAttribute("src","assets/Images/work3.jpg")
+  workfour.setAttribute("src","assets/Images/work4.jpg")
+  workfive.setAttribute("src","assets/Images/work5.jpg")
+  worksix.setAttribute("src","assets/Images/work6.jpg")
+  workone.style.visibility="visible"
+  worktwo.style.visibility="visible"
+  workthree.style.visibility="visible"
+  workfour.style.visibility="visible"
+  workfive.style.visibility="visible"
+  worksix.style.visibility="visible"
+}
+function com() {
+  workone.setAttribute("src","assets/Images/work2.jpg")
+  worktwo.setAttribute("src","assets/Images/work3.jpg")
+  workthree.setAttribute("src","assets/Images/work6.jpg")
+  workone.style.visibility="visible"
+  worktwo.style.visibility="visible"
+  workthree.style.visibility="visible"
+  workfour.style.visibility="hidden"
+  workfive.style.visibility="hidden"
+  worksix.style.visibility="hidden"
+}
+function rep() {
+  workone.setAttribute("src","assets/Images/work5.jpg")
+  workone.style.visibility="visible"
+  worktwo.style.visibility="hidden"
+  workthree.style.visibility="hidden"
+  workfour.style.visibility="hidden"
+  workfive.style.visibility="hidden"
+  worksix.style.visibility="hidden"
+}
+function res() {
+  workone.setAttribute("src","assets/Images/work1.jpg")
+  worktwo.setAttribute("src","assets/Images/work2.jpg")
+  workthree.setAttribute("src","assets/Images/work4.jpg")
+  workfour.setAttribute("src","assets/Images/work5.jpg")
+  workfive.setAttribute("src","assets/Images/work6.jpg")
+  workone.style.visibility="visible"
+  worktwo.style.visibility="visible"
+  workthree.style.visibility="visible"
+  workfour.style.visibility="visible"
+  workfive.style.visibility="visible"
+  worksix.style.visibility="hidden"
+}
